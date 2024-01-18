00e71226bfda0b9028336671292ec66fcec2d1dc
Dylan Tan Yuanzhun <84678991+dylantanyz@users.noreply.github.com>
2024-01-18 09:10:26 +0000

    Docker Networking

---
+++ pgcli.sh
@@ -0,0 +1,2 @@
+echo "Starting pgcli..."
+pgcli -h localhost -p 5432 -u root -d ny_taxi
---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Starting Postgres Container..."
+docker run -d \
+    -e POSTGRES_USER="root" \
+    -e POSTGRES_PASSWORD="root" \
+    -e POSTGRES_DB="ny_taxi" \
+    -v $(pwd)/ny_taxi_postgres_data:/var/lib/postgresql/data \
+    -p 5432:5432 \
+    postgres:13
+
+echo "Starting Jupyter Notebook..."
+jupyter notebook &
